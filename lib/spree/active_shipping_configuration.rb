35c98a085f550b5685a53b695eecb97da10074da
Andrew Shatnyy <andrew@moboom.com>
2015-07-15 17:56:30 -0700

    introduce valid preferences

--- lib/spree/active_shipping_configuration.rb
+++ lib/spree/active_shipping_configuration.rb
@@ -4,17 +4,21 @@ class Spree::ActiveShippingConfiguration < Spree::Preferences::Configuration
   preference :ups_password, :string, :default => "secret"
   preference :ups_key, :string, :default => "developer_key"
   preference :shipper_number, :string, :default => nil
+  preference :ups_valid, :boolean, :default => false
 
   preference :fedex_login, :string, :default => "meter_no"
   preference :fedex_password, :string, :default => "special_sha1_looking_thing_sent_via_email"
   preference :fedex_account, :string, :default => "account_no"
   preference :fedex_key, :string, :default => "authorization_key"
+  preference :fedex_valid, :boolean, :default => false
 
   preference :usps_login, :string, :default => "aunt_judy"
   preference :usps_commercial_base, :boolean, :default => false
   preference :usps_commercial_plus, :boolean, :default => false
+  preference :usps_valid, :boolean, :default => false
 
   preference :canada_post_login, :string, :default => "canada_post_login"
+  preference :canada_post_valid, :boolean, :default => false
 
   preference :units, :string, :default => "imperial"
   preference :unit_multiplier, :decimal, :default => 16 # 16 oz./lb - assumes variant weights are in lbs
